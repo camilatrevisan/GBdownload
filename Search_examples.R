58011b7c68f9e9980bc1f17ed5dd7dbc5873c487
camilatrevisan <33060927+camilatrevisan@users.noreply.github.com>
2017-12-24 17:47:15 -0300

    Exemplos de uso da função GBdownload

---
+++ Search_examples.R
@@ -0,0 +1,17 @@
+#Exemplos de busca que utilizam todas as possibilidades da funcao
+
+#Busca com 0 resultados
+GBdownload("Odontophrynus carvalhoi", "ND2")
+#Busca com ate 500 resultados
+GBdownload("Rhinella crucifer", "cytb")
+#Busca com 500 a 1000 resultados
+GBdownload("Leptodactylidae", "Coi")
+#Busca com 1000 a 1500 resultados
+GBdownload("Bufonidae", "Coi")
+#Busca com 1500 a 2000 resultados
+GBdownload("Hylidae", "Cytb")
+#busca com mais de 2000 resultados
+GBdownload("Hylidae", "Coi")
+#Busca com somente por especie
+GBdownload("Odontophrynus carvalhoi", "")
+
